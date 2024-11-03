2f02b2e2ecfc8dad13e0d7780b958caba6ba4ceb
Pedro Castro <106095810+peucastro@users.noreply.github.com>
2024-11-03 20:34:10 +0000

    Time: 3 ms (49.9%), Space: 8.6 MB (27.34%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false; 
+        unsigned long int rev = 0;
+        int tmp = x;
+
+        while (tmp != 0) {
+            rev = (rev * 10) + (tmp % 10);
+            tmp /= 10;
+        }
+
+        return rev == x;
+    }
+};
