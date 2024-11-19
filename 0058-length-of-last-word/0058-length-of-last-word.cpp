906dc783c0b333c455a979d27afbd19f1a370653
Pedro Castro <106095810+peucastro@users.noreply.github.com>
2024-11-19 23:18:23 +0000

    Time: 0 ms (100%), Space: 7.9 MB (85.99%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        if (s.empty()) return 0;
+    if (s.empty())
+        return 0;
 
-        char *p = &s.back();
-        while (*p == ' ')
-            p--;
+    int count = 0;
+    string::iterator p = s.end();
+    do {
+        p--;
+    } while (*p == ' ');
 
-        int count = 0;
-        while (p != &s.front() - 1 && *p != ' ') {
-            count++;
-            p--;
-        }
+    while (p >= s.begin() && *p != ' ') {
+        p--;
+        count++;
+    }
 
-        return count;
+    return count;
     }
 };
